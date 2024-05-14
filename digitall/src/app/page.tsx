d067614daf71a1aa8b9626690d048c22032407d2
Jan-Helsen1 <91271402+Jan-Helsen1@users.noreply.github.com>
2024-05-14 10:52:11 +0200

    initial commit

---
+++ digitall/src/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.scss";
+
+export default function Home() {
+  	return (
+    	<main className={styles.main}>
+			<h1 className={styles.header}>
+				Welcome to be included in this Digitall project!
+			</h1>
+			<h2>Coming soon to you!</h2>
+    	</main>
+  	);
+}
