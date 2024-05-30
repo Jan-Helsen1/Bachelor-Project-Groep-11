7125a08cfe3ebcd212ac8d2a1e9394a9ee2975f6
Jan-Helsen1 <r0795805@ucll.be>
2024-05-30 11:14:06 +0200

    added swagger to backend

--- backend/app.ts
+++ backend/app.ts
@@ -2,6 +2,8 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import singleRouter from './controller/single.routes';
 import multipleRouter from './controller/multiple.routes';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import cors from 'cors';
 
 const app = express();
@@ -10,11 +12,26 @@ const port = 3000
 app.use(cors({ origin: "http://localhost:5173" }))
 app.use(bodyParser.json());
 
+const swaggerOpts = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Back-end",
+        version: "1.0.0",
+      },
+    },
+    apis: ["./controller/*.routes.ts"],
+};
+const swaggerSpec = swaggerJsdoc(swaggerOpts);
+
 // Router for single url search
 app.use("/single", singleRouter);
 
 // Router for multiple url search
 app.use("/multiple", multipleRouter);
+
+// Swagger documentation
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(port, () => {
     console.log(`Back-end is running on port ${port}.`);
